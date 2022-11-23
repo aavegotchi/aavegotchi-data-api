8882e7171e3fbb0a4b7c03dbacd810d97e3859a1
Frank <frank.pfeift@protonmail.com>
2022-11-23 09:32:58 +0100

    fixed ghst token

--- helper/eth/provider.ts
+++ helper/eth/provider.ts
@@ -3,3 +3,7 @@ import { ethers } from "ethers";
 export const provider = new ethers.providers.JsonRpcProvider(
     "https://polygon-rpc.com"
 );
+
+export const mainnetProvider = new ethers.providers.JsonRpcProvider(
+    "https://mainnet.infura.io/v3/71e713fa8fa148c8b229b6e34a751005"
+);
